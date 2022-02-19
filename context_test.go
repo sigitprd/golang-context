0198e4d12e8d7127b4fbaced4d826c58203d0fba
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-19 19:48:02 +0700

    test context with timeout to make auto cancel in context sinyal

--- context_test.go
+++ context_test.go
@@ -55,6 +55,7 @@ func CreateCounter(ctx context.Context) chan int {
 			default:
 				destination <- counter
 				counter++
+				time.Sleep(1 * time.Second) // simulation slow
 			}
 		}
 	}()
@@ -79,6 +80,28 @@ func TestContextWithCancel(t *testing.T) {
 	cancel() // cancel the context
 
 	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter:", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	// time.Sleep(2 * time.Second)
 
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
